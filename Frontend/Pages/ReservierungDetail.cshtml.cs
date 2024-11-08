b4a799980b2f0334da26569ac0f47c31d361d32e
eliasgindl <elias.gindl@hak-feldbach.at>
2024-11-08 17:18:51 +0100

    Layout erweitert
    um ein paar Dinge...
    
    Co-Authored-By: Alexander Schmidt <153898442+alexanderschmidt2006@users.noreply.github.com>

---
+++ Frontend/Pages/ReservierungDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MaReSy2.Pages
+{
+    public class ReservierungDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Frontend/obj/Debug/net8.0/MaReSy2.AssemblyInfo.cs
+++ Frontend/obj/Debug/net8.0/MaReSy2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaReSy2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d8664fda0744f3f57420ec505af90cef1ba831")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+242dd043d4cf186746e6e34dc6c6c66450688666")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaReSy2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaReSy2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
