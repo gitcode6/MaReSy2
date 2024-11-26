6bf660d4b7d47d3cca750da08d21d1967a8d1d5d
alexanderschmidt2006 <alexander.schmidt@hak-feldbach.at>
2024-11-26 16:35:16 +0100

    SET Details + ZOffline

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -2,64 +2,52 @@ using MaReSy2.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+// Services hinzufügen
+builder.Services.AddRazorPages(options =>
+{
+    //options.Conventions.AuthorizeFolder("/"); // Schützt alle Seiten
+    //options.Conventions.AllowAnonymousToPage("/Login"); // Login explizit freigeben
+});
 builder.Services.AddAuthentication("MyCookieAuth")
     .AddCookie("MyCookieAuth", options =>
     {
-        options.LoginPath = "/Login"; // Setze den Pfad zur Login-Seite
+        options.LoginPath = "/Login"; // Pfad zur Login-Seite
     });
-
 builder.Services.AddAuthorization();
 builder.Services.AddHttpClient("API", client => { client.BaseAddress = new Uri("https://localhost:7162/api"); });
-
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<ProductService>();
 builder.Services.AddControllers();
 
-
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware konfigurieren
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
-// Füge die Authentifizierung und Autorisierung zur Pipeline hinzu
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
-
-
-
+// Routen registrieren
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapRazorPages();
+    endpoints.MapRazorPages(); // Razor Pages zuerst
+    endpoints.MapControllers(); // Controller danach
     endpoints.MapGet("/", async (context) =>
     {
         context.Response.Redirect("/Login");
     });
 });
 
-
-
-
-/*
-builder.Services.AddRazorPages(options =>
-{
-	options.Conventions.AuthorizeFolder("/"); // Schützt alle Seiten
-});
-*/
-
-
-
-
 app.Run();
+
