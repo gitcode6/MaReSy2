8ecff471fe3f720cddb82f04d0f7c69476543350
eliasgindl <elias.gindl@hak-feldbach.at>
2024-12-16 16:57:07 +0100

    SingleProducts: Liste + Delete

--- Frontend/Pages/LagerverwaltungEP.cshtml.cs
+++ Frontend/Pages/LagerverwaltungEP.cshtml.cs
@@ -1,12 +1,32 @@
+using MaReSy2.ConsumeModels;
+using MaReSy2.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MaReSy2.Pages
 {
+    
+
     public class LagerverwaltungEPModel : PageModel
     {
-        public void OnGet()
+        //???
+        private readonly SingleProductService _singleproductService;
+
+        public LagerverwaltungEPModel(SingleProductService singleProductService)
         {
+            _singleproductService = singleProductService;
+        }
+
+        public List<SingleProduct> singleProducts { get; private set; }
+        public async Task OnGetAsync()
+        {
+            singleProducts = await _singleproductService.GetSingleProductsAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            bool success = await _singleproductService.deleteSingleProductAsync(id);
+            return RedirectToPage();
         }
     }
 }
---
+++ Frontend/Services/SingleProductService.cs
@@ -0,0 +1,104 @@
+﻿using MaReSy2.ConsumeModels;
+using System.Diagnostics;
+using System.Text;
+
+namespace MaReSy2.Services
+{
+    public class SingleProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SingleProductService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<List<ConsumeModels.SingleProduct>> GetSingleProductsAsync()
+        {
+            var client = _httpClientFactory.CreateClient("API");
+
+
+            var response = await client.GetAsync("/api/singleproducts");
+
+            System.Diagnostics.Debug.WriteLine(response);
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                return System.Text.Json.JsonSerializer.Deserialize<List<SingleProduct>>(responseContent);
+
+            }
+            else { return new List<SingleProduct>(); }
+
+        }
+
+        public async Task<bool> addSingleProductAsync(SingleProduct singleProduct)
+        {
+            var client = _httpClientFactory.CreateClient("API");
+
+            string baseUrl = "/api/singleproducts";
+
+
+            using StringContent stringContent = new StringContent(System.Text.Json.JsonSerializer.Serialize(singleProduct), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(baseUrl, stringContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Debug.WriteLine(response.Content.ReadAsStringAsync());
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public async Task<bool> bearbeitenSingleProductAsync(SingleProduct singleProduct)
+        {
+            var client = _httpClientFactory.CreateClient("API");
+            string baseUrl = $"/api/singleproducts/{singleProduct.singleProductId}";
+
+            using StringContent stringContent = new StringContent(System.Text.Json.JsonSerializer.Serialize(singleProduct), Encoding.UTF8, "application/json");
+            var response = await client.PutAsync(baseUrl, stringContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Debug.WriteLine(response.Content.ReadAsStringAsync());
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<ConsumeModels.SingleProduct?> GetSingleProductAsync(int singleProductID)
+        {
+            var client = _httpClientFactory.CreateClient("API");
+
+            var response = await client.GetAsync($"/api/singleproducts/{singleProductID}");
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            System.Diagnostics.Debug.WriteLine(responseContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return System.Text.Json.JsonSerializer.Deserialize<SingleProduct>(responseContent);
+            }
+            else { return null; }
+        }
+
+        public async Task<bool> deleteSingleProductAsync(int singleProductID)
+        {
+            var client = _httpClientFactory.CreateClient("API");
+            string url = $"/api/singleproducts/{singleProductID}";
+
+            var response = await client.DeleteAsync(url);
+
+            return response.IsSuccessStatusCode;
+        }
+
+    }
+}
