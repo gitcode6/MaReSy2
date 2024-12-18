ab28dff816a84ff27eeb89193b46ce5eae79d11c
alexanderschmidt2006 <alexander.schmidt@hak-feldbach.at>
2024-12-18 10:51:00 +0100

    SingleProduct fertig

--- Frontend/ConsumeModels/CreateSingleProductModel.cs
+++ Frontend/ConsumeModels/CreateSingleProductModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace MaReSy2.ConsumeModels
 {
     public class CreateSingleProductModel
     {
+        
         public string singleProductName {  get; set; }
         public string singleProductSerialNumber { get; set; }
         public bool singleProductActive { get; set; }
