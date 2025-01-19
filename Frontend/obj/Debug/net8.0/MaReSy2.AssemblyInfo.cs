7bcf239df75f2871ae77fbed74efc5e3d94a699f
eliasgindl <elias.gindl@hak-feldbach.at>
2025-01-19 20:41:36 +0100

    FINAL1

--- Frontend/obj/Debug/net8.0/MaReSy2.AssemblyInfo.cs
+++ Frontend/obj/Debug/net8.0/MaReSy2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaReSy2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7db7fd76cec4365fca0112d3ac45734c23cec116")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+191dd694b392cfbde92793b77f41ee33e46d290c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaReSy2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaReSy2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Frontend/pdfReports/Produkte/ProdukteAuszugPdfGenerator.cs
+++ Frontend/pdfReports/Produkte/ProdukteAuszugPdfGenerator.cs
@@ -17,20 +17,81 @@ namespace MaReSy2.pdfReports.Produkte
             {
                 container.Page(page =>
                 {
-                    page.Margin(20);
-                    page.Size(PageSizes.A4);
-                    page.DefaultTextStyle(x => x.FontSize(12));
+                    // Seiteneinstellungen
+                    page.Margin(0);
+                    page.Size(PageSizes.A4.Landscape());
+                    //page.Size(PageSizes.A4);
+                    page.DefaultTextStyle(x => x.FontSize(12).FontFamily("Dunant"));
 
-                    page.Content().Column(column =>
+                    page.Header().Background("#403A60").AlignCenter().Text("Produkt-Liste").FontSize(24).Bold().FontColor("#FFFFFF").FontFamily("Dunant");
+
+                    page.Content().PaddingVertical(10).Column(column =>
                     {
-                        column.Item().Text("Produkte-Liste").FontSize(18).Bold().AlignCenter();
+                        column.Spacing(10); // Abstand zwischen den Elementen
 
-                        // Daten aus der API anzeigen
-                        foreach (var item in products)
+                        // Tabelle erstellen
+                        column.Item().Table(table =>
                         {
-                            column.Item().Text($"ID: {item.productId} - Name: {item.productname}");
-                        }
+                            // Spalten definieren
+                            table.ColumnsDefinition(columns =>
+                            {
+                                columns.RelativeColumn(10); // ID-Spalte
+                                columns.RelativeColumn(30);    // Name-Spalte
+                                columns.RelativeColumn(40);
+                                columns.RelativeColumn(20);
+                            });
+
+                            // Tabellenkopf
+                            table.Header(header =>
+                            {
+                                header.Cell().Background("#D6EAF8").Padding(5).Text("ID")
+                                    .FontSize(14).Bold().FontColor("#1B4F72");
+
+                                header.Cell().Background("#D6EAF8").Padding(5).Text("Name")
+                                    .FontSize(14).Bold().FontColor("#1B4F72");
+
+                                header.Cell().Background("#D6EAF8").Padding(5).Text("Beschreibung")
+                                    .FontSize(14).Bold().FontColor("#1B4F72");
+
+                                header.Cell().Background("#D6EAF8").Padding(5).Text("Status")
+                                    .FontSize(14).Bold().FontColor("#1B4F72");
+                            });
+
+                            // Zeilen
+                            foreach (var item in products)
+                            {
+                                string setActive = item.productactive ? "Aktiv" : "Inaktiv";
+                                table.Cell().Background("#F4F6F6").Padding(5).Text(item.productId.ToString());
+                                table.Cell().Background("#F4F6F6").Padding(5).Text(item.productname);
+
+                                table.Cell().Background("#F4F6F6").Padding(5).Text(item.productdescription);
+                                table.Cell().Background("#F4F6F6").Padding(5).Text(setActive);
+                            }
+                        });
+
+                        //page.Footer().Height(80).Column(column =>
+                        //{
+                        //    // Top Section: Logo
+                        //    column.Item().AlignCenter().Image(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "Logo_Österreichisches_Rotes_Kreuz_2.jpg")).FitWidth();
+
+                        //    // Bottom Red Bar with White Text
+                        //    column.Item()
+                        //        .PaddingTop(5)
+                        //        .Background(Colors.Red.Lighten1)
+                        //        .PaddingVertical(5)
+                        //        .Row(row =>
+                        //        {
+                        //            row.RelativeItem()
+                        //                .AlignCenter()
+                        //                .Text("ÖSTERREICHISCHES JUGENDROTKREUZ | WWW.JUGENDROTKREUZ.AT")
+                        //                .FontColor(Colors.White)
+                        //                .FontSize(9)
+                        //                .Bold();
+
+                        //        });
+                        //});
                     });
+
                 });
             }).GeneratePdf(memoryStream);
         }
